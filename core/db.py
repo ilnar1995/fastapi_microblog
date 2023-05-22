e05603bbdb8a2634568a75faecfbe5017f4c45bd
ilnar iskhakov <ilnar150595@gmail.com>
2023-05-22 10:40:28 +0300

    lesson_2

--- core/db.py
+++ core/db.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from starlette.requests import Request
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost/microblog"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db(request: Request):
+    return request.state.db
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import asyncio
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
+from core.db import SessionLocal
+
+from routes import routes
 
 app = FastAPI()
 
@@ -11,3 +16,17 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):   #для создания сессии при каждом запросе в api
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+    finally:
+        request.state.db.close()
+    return response
+
+
+app.include_router(routes)
+
