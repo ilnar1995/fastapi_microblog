e4cdf5bdc9bf6bd6f48881f81faefc892ee77712
ilnar iskhakov <ilnar150595@gmail.com>
2023-05-29 16:26:18 +0300

    добавлены перемен вирт-го окр-ия

--- core/db.py
+++ core/db.py
@@ -5,8 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from starlette.requests import Request
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:123456@localhost/microblog"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:123456@localhost/microblog"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
